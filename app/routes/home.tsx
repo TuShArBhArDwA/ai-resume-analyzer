a96e663a8a663b29968b9700b3797b05205f7c48
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2025-08-25 04:10:12 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,9 @@
-import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
-import {Link} from "react-router";
+import {usePuterStore} from "~/lib/puter";
+import {Link, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
@@ -11,6 +12,32 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth, kv } = usePuterStore();
+  const navigate = useNavigate();
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResumes, setLoadingResumes] = useState(false);
+
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
+  useEffect(() => {
+    const loadResumes = async () => {
+      setLoadingResumes(true);
+
+      const resumes = (await kv.list('resume:*', true)) as KVItem[];
+
+      const parsedResumes = resumes?.map((resume) => (
+          JSON.parse(resume.value) as Resume
+      ))
+
+      setResumes(parsedResumes || []);
+      setLoadingResumes(false);
+    }
+
+    loadResumes()
+  }, []);
+
   return <main className="bg-[url('images/bg-main.svg')] bg-cover">
     <Navbar/>
     <section className="main-section">
